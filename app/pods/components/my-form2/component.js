aa8f109e35bad0c9720b79294a2d132efa290af5
Jimmy <ultish@gmail.com>
2023-07-04 15:13:51 +1000

    whatever

--- app/pods/components/my-form2/component.js
+++ app/pods/components/my-form2/component.js
@@ -1,3 +1,33 @@
 import Component from '@ember/component';
 
 export default class MyForm2 extends Component {}
+const references = new WeakMap();
+const trackedChanges = new WeakMap();
+
+export function trackChanges(obj) {
+  let tracker = new Proxy(obj, {
+    get(target, prop) {
+      return Reflect.get(target, prop);
+    },
+    set(target, prop, value) {
+      let changes = trackedChanges.get(target) ?? new Map();
+      if (changes.get(prop) === value) {
+        changes.delete(prop);
+      } else if (!changes.has(prop)) {
+        changes.set(prop, Reflect.get(target, prop));
+      }
+      trackedChanges.set(target, changes);
+      return Reflect.set(target, prop, value);
+    },
+  });
+  references.set(tracker, obj);
+  return tracker;
+}
+
+export function changeSummary(obj) {
+  let reference = references.get(obj) ?? obj;
+  let changes = trackedChanges.get(reference) ?? new Map();
+  return [...changes].map(([prop, from]) => {
+    return { prop, from, to: reference[prop] };
+  });
+}
